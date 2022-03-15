1f3bff5ab8fe7d2a5be2920e6c9f44064b49bea7
Sergio Llopart Sotter <sergio.sotter@network-rg.com.br>
2022-03-14 22:02:57 -0300

    Backend Project Created

---
+++ backend/src/main/java/com/ssotter/sscatalog/SscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.ssotter.sscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SscatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssotter/sscatalog/SscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssotter.sscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
