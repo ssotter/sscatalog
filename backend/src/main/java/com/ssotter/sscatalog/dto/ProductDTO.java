57154652f4c434968b7e57c4f746f9c1ca15b5f8
Sergio Llopart Sotter <sergio.sotter@network-rg.com.br>
2022-06-28 14:01:54 -0300

    Basic beans validation

--- backend/src/main/java/com/ssotter/sscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/ssotter/sscatalog/dto/ProductDTO.java
@@ -6,6 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import com.ssotter.sscatalog.entities.Category;
 import com.ssotter.sscatalog.entities.Product;
 
@@ -13,10 +18,19 @@ public class ProductDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@Size(min = 5, max = 60, message = "Deve ter entre 5 e 60 caracteres")
+	@NotBlank(message = "Campo obrigatório")
 	private String name;
+	
+	@NotBlank(message = "Campo obrigatório")
 	private String description;
+	
+	@Positive(message = "O preço deve ter um valor positivo")
 	private Double price;
 	private String imgUrl;
+	
+	@PastOrPresent(message = "A data do produto não pode ser futura")
 	private Instant date;
 	
 	private List<CategoryDTO> categories = new ArrayList<>();
